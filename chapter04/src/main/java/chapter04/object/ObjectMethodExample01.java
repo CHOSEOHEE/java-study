95258f430f58c4fe468ff48e486296971e6dc142
CHOSEOHEE <whtjgml2002@naver.com>
2025-07-29 17:39:36 +0900

    Goods & Paint

--- chapter04/src/main/java/chapter04/object/ObjectMethodExample01.java
+++ chapter04/src/main/java/chapter04/object/ObjectMethodExample01.java
@@ -5,13 +5,13 @@ public class ObjectMethodExample01 {
 	public static void main(String[] args) {
 		Goods goods = new Goods();
 		 
-		Class klass = goods.getclass();       //reflection
+		Class klass = goods.getClass();       //reflection
 		System.out.println(klass.getName()); 
 		
-		System.out.println(goods.hashcode()); //address 기반의 해싱값
+		System.out.println(goods.hashCode()); //address 기반의 해싱값
 		                                      //address x
 		                                      //reference 값 x
-		System.out.println(goods.hashcodetoString()); //getClass().getName() + "@" + cashcode()
+		System.out.println(goods.toString()); //getClass().getName() + "@" + cashcode()
 		System.out.println(goods);
 
 	}
