91da11ce9a142936541d4074a133f010f2a101ba
CHOSEOHEE <whtjgml2002@naver.com>
2025-08-04 16:28:26 +0900

    netWork.html&webapp

---
+++ network/src/main/java/network/http/SimpleHttpServer.java
@@ -0,0 +1,45 @@
+package network.http;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleHttpServer {
+	private static final int PORT = 8088;
+
+	public static void main(String[] args) {
+
+		ServerSocket serverSocket = null;
+
+		try {
+			// 1. Create Server Socket
+			serverSocket = new ServerSocket();
+			   
+			// 2. Bind
+			
+			serverSocket.bind( new InetSocketAddress( "0.0.0.0", PORT ) );
+			consolLog("start...[port: " + PORT + "]");
+
+			while (true) {
+				Socket socket = serverSocket.accept();
+				new HttpRequestHandler(socket).start();
+			}
+
+		} catch (IOException ex) {
+			consolLog("error:" + ex);
+		} finally {
+			try {
+				if (serverSocket != null && serverSocket.isClosed() == false) {
+					serverSocket.close();
+				}
+			} catch (IOException ex) {
+				consolLog("error:" + ex);
+			}
+		}
+	}
+
+	public static void consolLog(String message) {
+		System.out.println("[HttpServer#" + Thread.currentThread().getId()  + "] " + message);
+	}
+}
